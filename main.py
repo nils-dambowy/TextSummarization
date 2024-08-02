4bad1fe72db62627f309f5aa30bde3245b516498
Nils <80416356+nils-dambowy@users.noreply.github.com>
2024-08-02 09:34:37 +0200

    project setup

---
+++ main.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+import os
+import torch
+import torchtext.data as data
+import torchtext.datasets as datasets
+import model
+import train
+
+
+# load dataset
+
+# load data
+
+# update args
+
+# model
+
+# train
---
+++ model.py
@@ -0,0 +1,8 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+
+class CNN_Text(nn.Module):
+    pass
